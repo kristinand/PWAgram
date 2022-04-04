60a2af10596d526e75138dfdb845a8999fe246cb
Kristina Andreeva <tabbyk@ya.ru>
2022-04-04 16:48:27 +0300

    add webpack and typescript

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'eval-cheap-source-map',
+  devServer: {
+    open: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
